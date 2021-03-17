d99faa0a33444c6f2fd62c5110bdbae34c6f8d56
AnkitaVK1997 <78569624+AnkitaVK1997@users.noreply.github.com>
2021-03-17 03:00:05 -0400

    Update run.py

--- run.py
+++ run.py
@@ -1,10 +1,30 @@
-from flask import Flask
+from flask import Flask,request
+from datetime import datetime
+
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello_world():
-    return 'Hello world!'
+@app.route('/date')
+def datee():
+    date = datetime.now()
+    d1 = date.strftime("%Y-%m-%d")
+    return d1, 200
+
+@app.route('/square')
+def square():
+    a = request.args.get('a')
+    sum=int(a)*int(a)
+    return str(sum), 200
+
+@app.route('/divide')
+def divide():
+    x=request.args.get('x')
+    y=request.args.get('y')
+    try:
+        div=float(x)/float(y)
+        return str(div),200
+    except:
+        return "Error", 400
 
 
 app.run(host='0.0.0.0',
